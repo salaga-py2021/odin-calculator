1d11c51ca66d122a45ae8a7b5553b1d42167c768
salaga-py2021 <pxg200018@utdallas.edu>
2022-07-06 21:26:03 -0500

    Add eventListener function that updates screen

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const buttons = document.querySelectorAll('button');
+const calcDisplay = document.getElementById('calcDisplay');
+let operandFlag = false;
+let clearFlag = false;
+let newCalc = false;
+let numberStorage = {0: '', 1: '', 2: ''};
+// --------------------------------------------------------------------------//
+
 function add(a,b){
     return a + b;
 }
@@ -11,13 +19,60 @@ function multiply(a,b){
 }
 
 function divide(a,b){
-    return (b != 0) ? a/b : "Only Chuck Norris May Divide by 0"
+    return (b != 0) ? a/b : "Only Chuck Norris May Divide by 0";
 }
 
 function operate(list){
     const operator = list[1];
-    if (operator == '+') {add(list[0],list[2])}
-    else if (operator == '-') {subtract(list[0],list[2])}
-    else if (operator == 'x') {multiply(list[0],list[2])}
-    else if (operator == '/') {divide(list[0],list[2])}
+    if (operator == '+') {return add(Number(list[0]),Number(list[2]));}
+    else if (operator == '-') {return subtract(list[0],list[2]);}
+    else if (operator == 'x') {return multiply(list[0],list[2]);}
+    else if (operator == '/') {return divide(list[0],list[2]);}
 }
+
+function updateDisplay(){
+    if(this.textContent.match(/[\*x\/+-]/gm) && this.textContent != '+/-'){
+        operandFlag = clearFlag = true;
+        if(newCalc){
+            newCalc = false;
+            numberStorage = {0: calcDisplay.textContent, 1: '', 2: ''};
+        }
+        numberStorage[1] = this.textContent;
+    }
+    else if (!isNaN(parseInt(this.textContent)) || this.textContent == '.'){ 
+        let temp = (operandFlag) ? 2 : 0;
+        if (clearFlag){
+            calcDisplay.textContent = '';
+            clearFlag = false;
+        }
+        if (newCalc){
+            numberStorage = {0: '', 1: '', 2: ''};
+            newCalc = false;
+        }
+        numberStorage[temp] += this.textContent;
+        calcDisplay.textContent += this.textContent;
+    }
+    else if(this.textContent == '='){
+        calcDisplay.textContent = operate(numberStorage);
+        clearFlag = newCalc = true;
+    }
+    else if(this.textContent == 'AC'){
+        calcDisplay.textContent = '';
+        numberStorage = {0: '', 1: '', 2: ''};
+        clearFlag = newCalc = operandFlag = false;
+    }
+    else if(this.textContent == '+/-'){
+        let temp = (operandFlag) ? 2 : 0;
+        numberStorage[temp] *= -1;
+        calcDisplay.textContent = numberStorage[temp];
+    }
+    else if(this.textContent == '%') {
+        let temp = (operandFlag) ? 2 : 0;
+        numberStorage[temp] /= 100;
+        calcDisplay.textContent = numberStorage[temp];
+    }
+}
+
+// --------------------------------------------------------------------------//
+
+buttons.forEach(button => button.addEventListener('click', updateDisplay))
